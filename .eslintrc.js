940a66fe9b1ecb0da79d29925d697ea301dfabda
Nikolay Kumanov <nikolay@TIDE-SOF-L0533.local>
2022-10-28 12:07:19 +0300

    add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": 0,
+    "no-underscore-dangle": "off",
+    "no-param-reassign": 0,
+    quotes: [2, "double", { avoidEscape: true }],
+    "no-return-await": 0,
+    "prefer-destructuring": ["error", { object: false, array: false }],
+    "no-use-before-define": ["error", { functions: false, classes: false }],
+  },
+};
